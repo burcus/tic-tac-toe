4f8e2ebeb91b8d5ecdf633924d7eed0f3c1f52ad
burcus <burcu.soylemez@bil.omu.edu.tr>
2022-03-28 21:29:05 +0300

    check is there a winner

--- lib/blocs/bloc_game.dart
+++ lib/blocs/bloc_game.dart
@@ -20,17 +20,48 @@ class BlocGame extends Bloc<EventGame, StateGame> {
   List<SquareStatus?> status = [];
 
   late Player currentPlayer;
+  late var gameBoard;
 
   Player get nextPlayer => currentPlayer == player1 ? player2 : player1;
 
+  void markGameBoard(SquareStatus status) {
+    int boardIndex = status.index + 1;
+    int rowIndex = boardIndex <= 3
+        ? 0
+        : boardIndex > 3 && boardIndex <= 6
+            ? 1
+            : 2;
+    int columnIndex = status.index % 3;
+    gameBoard[rowIndex][columnIndex] = currentPlayer.playerOrder == 1 ? 1 : 2;
+    print("current gameBoard = ${gameBoard}");
+  }
+
   void checkStatusList() {
-    List<Player?> players = status.take(3).map((e) => e?.player).toList();
+    bool isThereWinner = false;
+    for (List<int> row in gameBoard) {
+      if (row.every((column) => column == 1) ||
+          row.every((column) => column == 2)) {
+        isThereWinner = true;
+      }
+    }
+
+    for (int i = 0; i < gameBoard[0].lenght; i++) {
+      bool same = true;
+      for (int y = 0; i < gameBoard[0].length; y++) {
+        if (gameBoard[i][y] == 1)
+          break;
+        else {
+          same = false;
+        }
+      }
+    }
+    print(isThereWinner);
   }
 
   void mark(EventGameMark event, emit) {
     status[event.status.index] = event.status;
+    markGameBoard(event.status);
     checkStatusList();
-
     currentPlayer = nextPlayer;
     emit(StateGameMarked(status));
   }
@@ -38,5 +69,7 @@ class BlocGame extends Bloc<EventGame, StateGame> {
   void startParameters(event, state) {
     currentPlayer = player1;
     status = List.generate(9, (index) => null);
+    gameBoard = List.generate(3, (i) => List.filled(3, -1, growable: false),
+        growable: false);
   }
 }
